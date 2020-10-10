92b111a9f5e624d9060d2ca19580cbfa92829ee4
Ami <ami0275@gmail.com>
2020-10-10 09:24:54 -0700

    Api Data class.

---
+++ app/src/main/java/com/example/movie/model/Genre.kt
@@ -0,0 +1,6 @@
+package com.demoexoplayer.homeactivity.model
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/movie/model/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.demoexoplayer.homeactivity.model
+
+data class ProductionCompany(
+    val id: Int,
+    val logo_path: String,
+    val name: String,
+    val origin_country: String
+)
